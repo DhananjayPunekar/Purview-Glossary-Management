82be72f0a76404b6dc4eaacae8f79494b5f946ae
DhananjayPunekar <72909850+DhananjayPunekar@users.noreply.github.com>
2024-09-26 12:53:51 +0530

    Added the functionality to fetch the glossary term by term id or term name

--- main.py
+++ main.py
@@ -7,29 +7,30 @@ import config
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-try:
-    # Get authentication token
-    logging.info("Attempting to get Purview token...")
-    token = get_purview_token(config.tenant_id, config.client_id, config.client_secret, config.resource)
-    logging.info("Successfully obtained Purview token.")
+# try:
+# Get authentication token
+logging.info("Attempting to get Purview token...")
+token = get_purview_token(config.tenant_id, config.client_id, config.client_secret, config.resource)
+logging.info("Successfully obtained Purview token.")
 
-    # Set up headers with the token
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
+# Set up headers with the token
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
 
-    # Initialize glossary manager
-    logging.info("Initializing PurviewGlossaryManager...")
-    glossary_manager = PurviewGlossaryManager(token, config.tenant_id, headers)
-    logging.info("PurviewGlossaryManager initialized successfully.")
+# Initialize glossary manager
+logging.info("Initializing PurviewGlossaryManager...")
+glossary_manager = PurviewGlossaryManager(token, config.tenant_id, headers)
+logging.info("PurviewGlossaryManager initialized successfully.")
 
-    # Read glossary terms from Excel
-    logging.info("Reading glossary terms from Excel...")
-    glossary_terms = read_glossary_terms_from_excel('Enterprise-Glossary-Terms.xlsx')
-    logging.info(f"Successfully read {len(glossary_terms)} glossary terms from Excel.")
+# Read glossary terms from Excel
+logging.info("Reading glossary terms from Excel...")
+glossary_terms = read_glossary_terms_from_excel('Enterprise-Glossary-Terms.xlsx')
+logging.info(f"Successfully read {len(glossary_terms)} glossary terms from Excel.")
 
-    glossary_manager.upload_glossary_terms(glossary_terms)
+print(glossary_manager.get_term(term_id="4bf2fe0e-4768-40ba-b8d6-f5837c486e6f"))
+# print(glossary_manager.get_term(term_name="Accounts Payable (AP)"))
 
-except Exception as e:
-    logging.error(f"An error occurred: {str(e)}")
+# except Exception as e:
+#     logging.error(f"An error occurred: {str(e)}")
